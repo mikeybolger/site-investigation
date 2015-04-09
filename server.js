5725ae09e8e6cf499da4d95718fcd6d4e59393e4
mikeybolger <Mike@michael-bolgers-macbook.local>
2015-04-09 17:29:57 +0100

    added basic database and model

--- server.js
+++ server.js
@@ -27,3 +27,21 @@ app.get('/api/posts', function (req, res) {
 app.listen(3000, function () {
 	console.log('Listening on port', 3000)
 })
+
+app.post('/api/posts', function (req, res) {
+	console.log('post recieved')
+	console.log(req.body.fileRef)
+	console.log(req.body.prefix)
+	console.log(req.body.firstName)
+	console.log(req.body.lastName)
+	console.log(req.body.address)
+	console.log(req.body.townland)
+	console.log(req.body.phone)
+	console.log(req.body.fax)
+	console.log(req.body.email)
+	console.log(req.body.noResidents)
+	console.log(req.body.noDoubleBeds)
+	console.log(req.body.noSingleBeds)
+	console.log(req.body.waterSupply)
+
+})

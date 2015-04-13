b129708c8eb194edfcc5c96f465a4afab1bf920b
mikeybolger <Mike@michael-bolgers-macbook.local>
2015-04-13 10:04:55 +0100

    Integrated angular, node and mongo, data saved to database

--- server.js
+++ server.js
@@ -1,47 +1,22 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var logger = require('morgan')
 
 var app = express()
 app.use(bodyParser.json())
+app.use(logger('dev'))
+app.use(require('./controllers'))
+//app.use('/api/posts', require('./controllers/api/posts'))
+//app.use(require('./controllers/static'))
 
-app.get('/api/posts', function (req, res) {
-	res.json ([
-		{
-			fileRef: '67557776',
-			prefix: 'Mr',
-			firstName: 'John',
-			lastName: 'Doe',
-			address: 'Main Street',
-			townland: 'Some Town',
-			phone: '1234567',
-			fax: '7654321',
-			email: 'john@email.com',
-			noResidents: '4',
-			noDoubleBeds: '2',
-			noSingleBeds: '2',
-			waterSupply: 'well'
-		}
-	])
+
+var server = app.listen(3000, function () {
+	console.log('server listening on %d', server.address().port)
 })
+//app.use('/api/posts', require('./controllers/api/posts'))
+//app.use(require('./controllers/static'))
 
-app.listen(3000, function () {
-	console.log('Listening on port', 3000)
-})
 
-app.post('/api/posts', function (req, res) {
-	console.log('post recieved')
-	console.log(req.body.fileRef)
-	console.log(req.body.prefix)
-	console.log(req.body.firstName)
-	console.log(req.body.lastName)
-	console.log(req.body.address)
-	console.log(req.body.townland)
-	console.log(req.body.phone)
-	console.log(req.body.fax)
-	console.log(req.body.email)
-	console.log(req.body.noResidents)
-	console.log(req.body.noDoubleBeds)
-	console.log(req.body.noSingleBeds)
-	console.log(req.body.waterSupply)
-
-})
+//app.listen(3000, function () {
+//	console.log('Listening on port', 3000)
+//})

babf0d115c2e1fd1c8df1db46b950cadbb5e0d4e
mikeybolger <Mike@michael-bolgers-macbook.local>
2015-04-28 11:03:30 +0100

    updated text input

--- ng/routes.js
+++ ng/routes.js
@@ -1,7 +1,8 @@
 angular.module('app')
 .config(function ($routeProvider) {
   $routeProvider
-  .when('/',         { controller: 'PostsCtrl', templateUrl: '/templates/posts.html' })
-  .when('/register', { controller: 'RegisterCtrl', templateUrl: '/templates/register.html' })
-  .when('/login',    { controller: 'LoginCtrl', templateUrl: '/templates/login.html' })
+  .when('/',         	{ controller: 'PostsCtrl', templateUrl: '/templates/welcome.html' })
+  .when('/register', 	{ controller: 'RegisterCtrl', templateUrl: '/templates/register.html' })
+  .when('/login',    	{ controller: 'LoginCtrl', templateUrl: '/templates/login.html' })
+  .when('/posts',     	{ controller: 'PostsCtrl', templateUrl: '/templates/posts.html' })
 })

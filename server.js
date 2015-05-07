367bf13041aa4a22c47ab4caa3e4d981ac5f00b8
mikeybolger <Mike@michael-bolgers-macbook.local>
2015-05-07 16:36:29 +0100

    added webRTC conference room

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var logger = require('morgan')
 
+var static = require('node-static');
+//var http = require('http');
+var file = new(static.Server)();
+//var app = http.createServer(function (req, res) {
+  //file.serve(req, res);
+//}).listen(2013);
+
 var app = express()
 app.use(bodyParser.json())
 app.use(logger('dev'))
@@ -15,6 +22,13 @@ var server = app.listen(3000, function () {
 })
 //app.use('/api/posts', require('./controllers/api/posts'))
 //app.use(require('./controllers/static'))
+
+//var static = require('node-static');
+//var http = require('http');
+//var file = new(static.Server)();
+//var app = http.createServer(function (req, res) {
+  //file.serve(req, res);
+//}).listen(2013);
 
 
 //app.listen(3000, function () {
